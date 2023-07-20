bcb5b84f4fab261a4d7ea69b04c7b62fe71b78d5
Adam Olej <51245883+Adesz88@users.noreply.github.com>
2023-07-20 16:46:59 +0200

    Added expiry check

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,15 +1,16 @@
-import { Component, Input, OnChanges, SimpleChanges, numberAttribute } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges, numberAttribute } from '@angular/core';
 import { CategoryService } from '../shared/services/category.service';
 import { TodoService } from '../shared/services/todo.service';
 import { Todo } from '../shared/models/Todo';
-import { faTrashCan, faPenToSquare } from '@fortawesome/free-solid-svg-icons';
+import { faTrashCan, faPenToSquare} from '@fortawesome/free-solid-svg-icons';
+import { Subscription, map, timer } from 'rxjs';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent implements OnChanges{
+export class MainComponent implements OnChanges, OnInit, OnDestroy{
   @Input() filter: string = "all";
 
   faTrashCan = faTrashCan;
@@ -19,6 +20,8 @@ export class MainComponent implements OnChanges{
   showAddEditModal: boolean = false;
   showSettingsModal: boolean = false;
 
+  timerSubscription: Subscription = new Subscription;
+
   constructor(private categoryService: CategoryService, private todoService: TodoService) {
     let categories = categoryService.getAll();
     console.log(categories);
@@ -27,8 +30,21 @@ export class MainComponent implements OnChanges{
     console.log(this.todos);
   }
 
+  ngOnInit(): void {
+    this.timerSubscription = timer(0, 60000).pipe(
+      map(() => {
+        this.todos = this.todoService.getAll(this.filter);
+        console.log(new Date);
+      })
+    ).subscribe();
+  }
+
   ngOnChanges(changes: SimpleChanges): void {
     this.todos = this.todoService.getAll(this.filter);
+  }
+
+  ngOnDestroy(): void {
+    this.timerSubscription.unsubscribe();
   }
 
   onTodoCheck(todo: Todo) {
@@ -49,13 +65,11 @@ export class MainComponent implements OnChanges{
   }
 
   onEdit(todo: Todo) {
-    console.log("edit" + todo.id);
     this.currentTodo = todo;
     this.showAddEditModal = true;
   }
 
   onDelete(id: number) {
-    console.log("delete" + id);
     this.todoService.delete(id);
     this.todos = this.todoService.getAll(this.filter);
   }
@@ -67,5 +81,26 @@ export class MainComponent implements OnChanges{
 
   settingsOnClose(save: boolean) {
     this.showSettingsModal = false;
+  }
+
+  isExpired(todo: Todo) {
+    const now = new Date();
+    const todoDate = new Date(todo.date);
+
+    if (todoDate.getFullYear() < now.getFullYear()) { return true; }
+    if (todoDate.getFullYear() === now.getFullYear() && todoDate.getMonth() < now.getMonth()) { return true; }
+    if (todoDate.getFullYear() === now.getFullYear() && todoDate.getMonth() === now.getMonth()
+        && todoDate.getDay() < now.getDay()) { return true; }
+    
+    if (todoDate.getFullYear() === now.getFullYear() && todoDate.getMonth() === now.getMonth()
+        && todoDate.getDay() === now.getDay() && todo.time) {
+
+      const todoHours = Number(todo.time?.substring(0, 2));
+      const todoMinutes = Number(todo.time?.substring(3, 5));
+      if (todoHours < now.getHours()) { return true; }
+      if (todoHours === now.getHours() && todoMinutes < now.getMinutes()) { return true; }
+    }    
+
+    return false;
   }
 }
